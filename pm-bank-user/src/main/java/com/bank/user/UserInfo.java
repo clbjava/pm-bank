d0be65363487e583c9dce06293562e551b0c8e9f
clbjava <295435732@qq.com>
2020-05-25 23:35:22 +0800

    s

--- pm-bank-user/src/main/java/com/bank/user/UserInfo.java
+++ pm-bank-user/src/main/java/com/bank/user/UserInfo.java
@@ -1,4 +1,13 @@
 package com.bank.user;
 
 public class UserInfo {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
